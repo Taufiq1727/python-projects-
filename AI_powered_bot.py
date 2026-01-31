fa695d779600b51a2cb68b15511fa2871888c551
Taufiq <tm8023009@gmail.com>
2026-01-31 20:59:37 +0530

    Add files via upload
    adding my new project ai bot

---
+++ AI_powered_bot.py
@@ -0,0 +1,34 @@
+from openai import OpenAI
+
+key = #Can't just write my api keyt asi
+
+messages = []
+
+client = OpenAI(
+    api_key = key,
+)
+def completion(message):
+    global messages 
+    messages.append(
+        {
+            "role": "user",
+            "content": message,
+        }
+    )
+    chat_completion = client.chat.completions.create(messages = messages,model = "gpt-4o")
+    
+
+    # print(chat_completion)
+    message = {
+        "role" : "assistant",
+        "content": chat_completion.choices[0].message.content
+    }
+    messages.append(message)
+    print(f"Khushi : {message["content"]}")
+
+if __name__ == "__main__":
+    print(f"Khushi :Hi I am Khushi, How may I help you\n")
+    while True:
+        user_question = input()
+        print(f"User : {user_question}")
+        completion(user_question)
