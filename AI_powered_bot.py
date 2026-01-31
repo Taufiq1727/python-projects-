bfa980eb73fbc50fac5a70b70a99605a1b886057
Taufiq <tm8023009@gmail.com>
2026-01-31 22:10:05 +0530

    Update API key placeholder and format code

--- AI_powered_bot.py
+++ AI_powered_bot.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 
-key = #Can't just write my api key as this is uploaded in git hub
+key = #Enter the API key of open ai
 
 messages = []
 
@@ -32,4 +32,5 @@ if __name__ == "__main__":
         user_question = input()
         print(f"User : {user_question}")
         completion(user_question)
+
 
